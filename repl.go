c69e96ca3e7b067168f9ffa2956b6e72a4579268
szuter <piotrstrzelec1989@gmail.com>
2025-02-06 10:41:40 +0100

    Add pokedex command to CLI for viewing all caught Pokemon

--- repl.go
+++ repl.go
@@ -97,5 +97,10 @@ func getCommands() map[string]cliCommand {
 			description: "View details about a caught Pokemon",
 			callback:    commandInspect,
 		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "View all caught Pokemon",
+			callback:    commandPokedex,
+		},
 	}
 }
